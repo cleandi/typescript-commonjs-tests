886b7ca819c8acb834c437889977645056063367
Daniel Garcia <dg2@aderant.com>
2021-08-20 19:45:20 +0200

    initial commit

---
+++ test.ts
@@ -0,0 +1,14 @@
+import {builder} from "cleandi";
+
+type P = {
+    msg: string;
+}
+const provider = builder<P>()
+    .bindValue('msg', 'hello world!')
+    .build('msg');
+
+const isOk = provider.msg === 'hello world!';
+if (isOk)
+    process.exit(0)
+else
+    process.exit(1);
